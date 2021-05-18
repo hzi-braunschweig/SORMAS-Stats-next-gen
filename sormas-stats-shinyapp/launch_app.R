a2ba4a4e21552236fb2607db9615ff589c4b973a
Silelou Bernard Chawo <bernard.silenou@helmholtz-hzi.de>
2021-05-18 02:44:47 +0200

    Delete launch_app.R

--- sormas-stats-shinyapp/launch_app.R
+++
@@ -1,9 +0,0 @@
-
-mypath = "/home/bsi17/Dropbox/sormas_2021/sormasDevelopment/dashboardShiny/sormas/sormasApp" # This should be the address of the shinyapp folder of your sustem
-#sep = ";"
-setwd(mypath)
-source("loading_packages.R")
-source("loading_functions.R")
-source("loading_data.R" )
-runApp(appDir = mypath, launch.browser = TRUE, host = "127.0.0.1")
-
