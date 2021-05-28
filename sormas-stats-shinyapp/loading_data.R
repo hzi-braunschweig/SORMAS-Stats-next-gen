44f319fdbfce5e652f59e543962af1bbf9aa2439
bernardsilenou <bernard.silenou@helmholtz-hzi.de>
2021-05-28 21:01:12 +0200

    [#25] Transfrom node and elist data in form needed by graph_from_data_frame
    This bug is possibly caused by the fact that the the frist columns on elist and node list should be the varaibles needed by graph_from_data_frame

--- sormas-stats-shinyapp/loading_data.R
+++ sormas-stats-shinyapp/loading_data.R
@@ -1,39 +1,8 @@
-### The codes in this file connnect to sormas db and import the required data into R
 
-# Defining parameters to be used to sample data
-delay = 1000 # number of days to count backward from today,  default from date = 90 days in the past
-fromDate = as.character(Sys.Date() - delay - 1) #  you can directly define fromDate as: fromDate = as.character("yyyy-mm-dd")
-toDate = as.character(Sys.Date() + 1) # or toDate = as.character("yyyy-mm-dd"), +1 is added because between sql commant does not consider end of intervals
-uniquePersonPersonContact = TRUE # or FALSE to keep only one contact between the same 2 persons ( case person and contact person)
-
-# Defining colours to be used by dashboard icons for entities
-colCont = "green"  # contact colour
-colCase="red"   # case colour
-colEvent = "blue"   # event colour
-colPerson = "black"   # contact person or event participant person colour
-colEdge = "black" 
-
-# Defining connection to db
-DB_USER = "sormas_user"
-DB_PASS = "password"
-DB_HOST = "127.0.0.1"
-DB_PORT = "5432"
-DB_NAME= "sormas"
 
 ## Extracting eventData -----
 sormas_db = dbConnect(PostgreSQL(), user=DB_USER,  dbname=DB_NAME, password = DB_PASS, host=DB_HOST, port=DB_PORT)
-eventData = eventExport(sormas_db,fromDate = fromDate, toDate = toDate)
-
-
-
-# Defining parameters to be used to sample case and contact data
-delay = 700 # number of days to count backward from today,  default from date = 90 days in the past
-fromDate = as.character(Sys.Date() - delay - 1) #  you can directly define fromDate as: fromDate = as.character("yyyy-mm-dd")
-toDate = as.character(Sys.Date() + 1) # or toDate = as.character("yyyy-mm-dd"), +1 is added because between sql commant does not consider end of intervals
-uniquePersonPersonContact = TRUE # or FALSE to keep only one contact between the same 2 persons ( case person and contact person)
-
-
-
+eventData = eventExport(sormas_db,fromDate = event_fromDate, toDate = event_toDate)
 
 ## Extracting infectorInfecteeData -----
 infectorInfecteeData = infectorInfecteeExport(sormas_db, fromDate = fromDate, toDate = toDate)
