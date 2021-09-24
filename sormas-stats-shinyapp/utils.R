7afe7f4e546a6ce29f2bdea72d05e71df3f437a3
bernardsilenou <bernard.silenou@helmholtz-hzi.de>
2021-09-24 15:59:29 +0200

    [#70]New script for location_category_fr

--- sormas-stats-shinyapp/utils.R
+++ sormas-stats-shinyapp/utils.R
@@ -1279,6 +1279,7 @@ compute_eventlocation_category= function(eventData){
     dplyr::mutate(
       location_category = case_when (
         typeofplace_event =='FACILITY' & facilitytype_location =='ASSOCIATION' ~ 'ASSOCIATION',
+        typeofplace_event =='FACILITY' & facilitytype_location =='SWIMMING_POOL' ~ 'SWIMMING_POOL', 
         typeofplace_event == 'FACILITY' & facilitytype_location == 'THEATER' ~ 'THEATER',
         typeofplace_event == 'FACILITY' & facilitytype_location %in% c('SCHOOL', "KINDERGARTEN", "AFTER_SCHOOL") ~ 'SCHOOL',
         typeofplace_event == 'FACILITY' & facilitytype_location %in% c('HOSPITAL',  "DAY_HOSPITAL") ~ 'HOSPITAL',
@@ -1291,6 +1292,7 @@ compute_eventlocation_category= function(eventData){
         typeofplace_event == 'FACILITY' & facilitytype_location == 'RESTAURANT' ~ 'RESTAURANT',
         typeofplace_event == 'FACILITY' & facilitytype_location == 'CHILDRENS_DAY_CARE' ~ 'CHILDRENS_DAY_CARE',
         typeofplace_event == 'FACILITY' & facilitytype_location == 'BAR' ~ 'BAR',
+        typeofplace_event == 'FACILITY' & facilitytype_location == 'NIGHT_CLUB' ~ 'NIGHT_CLUB',   
         typeofplace_event == 'FACILITY' & facilitytype_location %in% c('OTHER_RESIDENCE', "HOMELESS_SHELTER", "RETIREMENT_HOME", "REFUGEE_ACCOMMODATION") ~ 'OTHER_RESIDENCE',
         typeofplace_event == 'FACILITY' & facilitytype_location == 'CORRECTIONAL_FACILITY' ~ 'CORRECTIONAL_FACILITY',
         typeofplace_event == 'FACILITY' & facilitytype_location == 'CHILDRENS_HOME' ~ 'CHILDRENS_HOME',
@@ -1299,25 +1301,24 @@ compute_eventlocation_category= function(eventData){
         typeofplace_event == 'PUBLIC_PLACE'  ~ 'PUBLIC_PLACE',
         TRUE ~ 'UNDEFINED')) %>%  # renaming french categories
     dplyr::mutate(location_category_fr = case_when(
-      location_category == "ASSOCIATION" ~  'ACES-Act/Ev.sport', #  #ACES - Activité ou évènement sportif
+      location_category %in% c("ASSOCIATION", "SWIMMING_POOL") ~  'ACES-Act/Ev.sport', #  #ACES - Activité ou évènement sportif
       location_category == "THEATER" ~  'ACEC-Act/Ev.cult',   #  'ACEC - Activité ou évènement culturel'
       location_category == "SCHOOL" ~ "SCOL-Milieu.scol" ,  #SCOL - Évènement ou situation en milieu scolaire
       location_category == "HOSPITAL" ~ "ESPP-Etab.sanit.", # ESPP – Etablissements de santé publics ou privés
       location_category == "MO_DIS_ELDER" ~ "EMES-Etab.MS" ,
       location_category == "UNIVERSITY"  ~ "UNIV-Ev/Sit.enseig.sup" , # 'UNIV - Évènement ou situation en établissement d_enseignement supérieur'
-      location_category == "OTH_MED_FACILITY" ~ "AUTR_ESPP-Aut.etab.sanit", #'AUTR - Autres lieu ou évènement sans respect des gestes barrières', should this be other in sromas or other not mentioned in your type of medical facility
       location_category == "BUSINESS" ~ "RAEP-EV.act.prof" ,    # 'RAEP - Rassemblement ou évènement en lien avec une activité professionnelle', Can there not be more options here?
       location_category == "HOTEL" ~ "TOUR-Hébergement.tour", # TOUR- Structure d_hébergement touristique
       location_category == "CANTINE" ~ "RCMP-Rest.col.mil.prof)" ,  # RCMP - Restauration collective en milieu  professionnel
       location_category == "RESTAURANT" ~ "REST-Restaurant" ,
       location_category == "CHILDRENS_DAY_CARE" ~ "CRCH-Struct.acc.jeune.enf" ,  #CRCH  - Structure accueil du jeune enfant
-      location_category == "BAR" ~ "BARS" ,
+      location_category %in% c("BAR", "NIGHT_CLUB") ~ "BARS" ,
       location_category == "OTHER_RESIDENCE" ~ "FROP-Foyer.résid/pens", # FROP - Foyer de résidence ou pensionnat (jeunes, travailleurs) 
       location_category == "CORRECTIONAL_FACILITY" ~ "EPEN-Étab.péni" , # EPEN - Établissement pénitentiaire
       location_category == "CHILDRENS_HOME" ~ "HPJJ-Struct.relevant.PJJ" ,    #HPJJ  -  Structure accueil et hébergement relevant de la PJJ
       location_category == "MEANS_OF_TRANSPORT" ~ "PPTC-transp.col" ,  # PPTC - présence prolongée dans un transport collectif
       location_category == "HOME" ~ "RPFA-Réunions.famil/amic" ,   # RPFA - Réunions privées dans un cadre familial, amical ou autre
-      location_category == "PUBLIC_PLACE" ~ "AUTR-Autres" ,
+      location_category %in% c("PUBLIC_PLACE", "OTH_MED_FACILITY") ~ "AUTR-Autres" ,
       location_category == "UNDEFINED" ~  'INDEFINIE'))
   return(ret)
 }
