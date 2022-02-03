18b994802870dd99866d38d0ef411b96563c2b96
DavidSoSiZoch <david.zoch@lessing-ffm.net>
2022-02-03 18:36:06 +0100

    GetEpidBase set up part for cases

--- sormas-stats-sitrep/sitrep-app/utils/GetEpidBase.R
+++ sormas-stats-sitrep/sitrep-app/utils/GetEpidBase.R
@@ -19,17 +19,39 @@
 #'
 #' @return Returns the epidemic dynamic base table
 #' @export
+#' 
+#' @seealso [AggregateCountsByVariable()]
 #'
 #' @examples
-GetEpidBase <- function(data_line_list = case_data_list){
+GetEpidBase <- function(data_line_list = case_data_line_list,
+                        toDate = toDate){
+  
+  # Define latest date
+  latest_date <- "2021-04-10" # as.character(as.Date(toDate)-1) #  example 
   
   # Get dataframe with base indicators for cases
   
   ## Get total cases per district
+  total_cases_district <- AggregateCountsByVariable(data_line_list = data_line_list,
+                                                    count_values = "caseclassification_case",
+                                                    by_variable = "id_district") %>% 
+    dplyr::rename_with(.cols = !id_district, function(x){paste0("TOT_", x)})
   
   ## Get new cases per district
+  ### First filter data for new cases based on reporting date
+  new_cases_district <- data_line_list %>% 
+    dplyr::filter(report_date_case == latest_date)  %>% 
+    AggregateCountsByVariable(data_line_list = .,
+                              count_values = "caseclassification_case",
+                              by_variable = "id_district") %>%
+    dplyr::rename_with(.cols = !id_district, function(x){paste0("NEW_", x)})
   
-  ## Join on id_district
+  ## Join on id_district and summarize confirmed cases
+  
+  epid_base_cases <- total_cases_district %>% 
+    dplyr::full_join(new_cases_district) %>% 
+    dplyr::mutate(TOTAL_CONFIRMED = rowSums(across(starts_with("TOT_CONFIRMED")), na.rm = TRUE)) %>% 
+    dplyr::mutate(TOTAL_NEW = rowSums(across(starts_with("NEW_CONFIRMED")), na.rm = TRUE))
   
   ###########################
   
@@ -53,7 +75,8 @@ GetEpidBase <- function(data_line_list = case_data_list){
   
   ###########################
   
-  # Join the 3 dataframes on id_district (full join)
+  # Join the 3 dataframes for cases, hospitalizations and deaths
+  # on id_district (full join)
   
   
  return(epid_base) 
