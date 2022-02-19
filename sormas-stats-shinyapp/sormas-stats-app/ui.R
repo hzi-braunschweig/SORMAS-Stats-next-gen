ccc3ed7d78f793d3ebbd9e3d70543ee39aa9d7a6
bernardsilenou <bernard.silenou@helmholtz-hzi.de>
2022-02-19 17:28:34 +0100

    deleted error in twoByTwoTablefunction

--- sormas-stats-shinyapp/sormas-stats-app/ui.R
+++ sormas-stats-shinyapp/sormas-stats-app/ui.R
@@ -681,8 +681,8 @@ shinyUI(bootstrapPage(
                                                                      width = 15,
                                                                      #height = 158, # 118, # 142,
                                                                      div( DT::dataTableOutput("k_estimate_table", width = "100%", height = "auto"), style = "font-size: 100%; width: 100%"),
-                                                                     h5(helpText("SD: standard deviation, CI: confidence interval, k > 0 implies overdispersion,  k < 0 implies overdispersion,
-                                                                                  a lower dispersion parameter indicates  higher transmission heterogeneity."))
+                                                                     h5(helpText("SD: standard deviation, CI: confidence interval, a lower dispersion parameter k indicates  higher transmission heterogeneity
+                                                                                 i.e. more transmissions resulted from a small number of people."))
                                                                      )
                                                                  )
                                                                )
