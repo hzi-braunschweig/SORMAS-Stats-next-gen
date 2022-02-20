7feb1f5163536b4a2fac54af6bdc22db1e3d96fb
DavidSoSiZoch <david.zoch@lessing-ffm.net>
2022-02-20 20:13:11 +0100

    inserte polaceholder for report date in Rmd, added some comments during discussion

--- sormas-stats-sitrep/sitrep-app/utils/GetNewCountsPerDistrict.R
+++ sormas-stats-sitrep/sitrep-app/utils/GetNewCountsPerDistrict.R
@@ -12,10 +12,12 @@
 #' @export
 #'
 #' @examples
-GetNewCountsPerDistrict <- function(variable = "caseclassification_case", date_type = "report_date_case"){
+GetNewCountsPerDistrict <- function(variable = "caseclassification_case",
+                                    date_type = "report_date_case"
+                                    ){
   
   # Define latest date
-  latest_date <- c(Sys.Date(), Sys.Date()-1) # as.character(as.Date(toDate)-1) #  example 
+  latest_date <- c(Sys.Date() -1, Sys.Date()) # as.character(as.Date(toDate)-1)
   
   ### First filter data for new counts on date type
   new_counts_district <- case_data_line_list %>% 
