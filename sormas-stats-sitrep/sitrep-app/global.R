c9468b8daa916992a157a7676d072445eca38526
DavidSoSiZoch <david.zoch@lessing-ffm.net>
2022-02-23 20:01:03 +0100

    now render SitRep.Rmd from global.r, reportDate is displayed on rendered SitRep

--- sormas-stats-sitrep/sitrep-app/global.R
+++ sormas-stats-sitrep/sitrep-app/global.R
@@ -26,3 +26,6 @@ base::source(file.path(".", "loading_functions.R"))
 ## loading data ----
 ## loading_data connects to sormas db, pull all the non sensitive data needed by sormas-stats and disconnect when done
 base::source(file.path(".", "loading_data.R"))
+
+## knit Situation Report Rmd file
+rmarkdown::render(file.path(".", "SitRep.Rmd"))
