a9f1920193dee66543605bf6944353efb1164176
DavidSoSiZoch <david.zoch@lessing-ffm.net>
2022-02-23 18:02:03 +0100

    changed functions to return tables (GetOverviewTable & GetRegionEpidTable) instead of displaying table directly, added GetTextTableKeys to get text table keys for dynamic text

--- sormas-stats-sitrep/sitrep-app/utils/GetRegionEpidTable.R
+++ sormas-stats-sitrep/sitrep-app/utils/GetRegionEpidTable.R
@@ -1,31 +1,32 @@
-#' Display table of country level epidemic indicators overview
+#' Get table of region level epidemic indicators
 #'
 #' @param epid_data Output of the GetEpidBase() function. 
-#'
+#' 
 #' @return Returns an overview table of the following 6 indicators
-#' on country level:
+#' on regional level:
 #'  - TOTAL CONFIRMED CASES
 #'  - NEW CONFIRMED CASES
 #'  - TOTAL HOSPITALIZATIONS
 #'  - NEW HOSPITALIZATIONS
 #'  - TOTAL DEATHS
 #'  - NEW DEATHS
-#'  
+#' 
 #' @seealso [GetEpidBase()]
 #' @export
 #'
 #' @examples
-DisplayOverviewEpidTable <- function(epid_data){
-  ## building country overview epid table
-  overview_epid_table <- epid_data %>% 
+GetRegionEpidTable <- function(epid_data){
+  ## building region epid table
+  region_epid_table <- epid_data %>% 
+    dplyr::group_by(name_region) %>% 
     dplyr::summarise("Total cases" = sum(TOTAL_CONFIRMED_CASES),
                      "New cases" = sum(TOTAL_NEW_CONFIRMED_CASES),
                      "Total hospitalizations" = sum(HOSP_YES),
                      "New hospitalizations" = sum(NEW_HOSP_YES),
                      "Total deaths" = sum(DEATH_EPIDEMIC_DISEASE),
-                     "New deaths" = sum(NEW_DEATH_EPIDEMIC_DISEASE))
-  
-  # displaying table
-  flextable::flextable(overview_epid_table)
-  
+                     "New deaths" = sum(NEW_DEATH_EPIDEMIC_DISEASE)) %>% 
+    dplyr::rename(Region = name_region)
+
+  # return regional epid table 
+  return(region_epid_table)
 }
---
+++ sormas-stats-sitrep/sitrep-app/utils/GetTextTableKeys.R
@@ -0,0 +1,73 @@
+#' Get text keys for table
+#'
+#' @param overview_table Output of the GetOverviewEpidTable().
+#' @param region_table Output of the GetRegionEpidTable().
+#'
+#' @return Returns a vector with the table text keys for the overview and 
+#' region tables.
+#' @export
+#'
+#' @examples
+GetTextTableKeys <- function(overview_table, region_table){
+  
+  ########### OVERVIEW TEXT TABLE KEYS ###########################
+  
+  
+  # column keys for overview table
+  column_keys_overview <-c("#Overview_TC",
+                           "#Overview_NC",
+                           "#Overview_TH",
+                           "#Overview_NH",
+                           "#Overview_TD",
+                           "#Overview_ND") 
+  
+  # overview table keys
+  list_overview_table_keys <- rep(list(column_keys_overview), times = nrow(overview_table))
+  
+  # get row counts to complete overview table keys
+  for (i in 1:length(list_overview_table_keys)){
+    vector_row <- list_overview_table_keys[[i]]
+    # add number indicating table row
+    vector_row <- paste0(vector_row,"_", i)
+    # reassign vector to list
+    list_overview_table_keys[[i]] <- vector_row
+  }
+  
+  # merge list of region table keys into one vector
+  overview_table_keys <- Reduce(c, list_overview_table_keys)
+ 
+  
+  ######### REGION TEXT TABLE KEYS ####################################
+  
+   
+  # column keys for region table
+  column_keys_region <-c("#Region_TC",
+                           "#Region_NC",
+                           "#Region_TH",
+                           "#Region_NH",
+                           "#Region_TD",
+                           "#Region_ND")
+  
+  # region table keys
+  list_region_table_keys <- rep(list(column_keys_region), times = nrow(region_table))
+  
+  # get row counts to complete overview table keys
+  for (i in 1:length(list_region_table_keys)){
+    vector_row <- list_region_table_keys[[i]]
+    # add number indicating table row
+    vector_row <- paste0(vector_row,"_", i)
+    # reassign vector to list
+    list_region_table_keys[[i]] <- vector_row
+  }
+  
+  # merge list of region table keys into one vector
+  region_table_keys <- Reduce(c, list_region_table_keys)
+  
+  
+  #################### MERGING #########################################
+  
+  # merge overview_table_keys and region_table_keys into one long vector: text_table_keys
+  text_table_keys <- c(overview_table_keys, region_table_keys)
+
+  return(list())
+}
