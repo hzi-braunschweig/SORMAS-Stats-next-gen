3dfb762df29750b3e16e861cb71fad41955e683b
DavidSoSiZoch <david.zoch@lessing-ffm.net>
2022-02-23 09:38:25 +0100

    implemented line_color argument into TimeseriesGraph and fixing problem where two line would be displayed on top of each other

--- sormas-stats-sitrep/sitrep-app/utils/DisplayTimeseriesGraphs.R
+++ sormas-stats-sitrep/sitrep-app/utils/DisplayTimeseriesGraphs.R
@@ -7,7 +7,7 @@
 #' @param timeseries_data Outout of the GetTimeseriesData() function.
 #' 
 #' @param colors A vector of three strings. Defined the colors of the three 
-#' graphs. Must be colors from the ggplot color palette.
+#' graphs. Must be colors recognized by gglpot.
 #' Defaults to colors = c("steelblue","darkred","#E69F00").
 #'
 #' @return A plot containing the following three timeseries graphs:
@@ -28,20 +28,20 @@ DisplayTimeseriesGraphs <- function(timeseries_data,
   # get timeseries graph for confirmed cases by report date
   cases_by_report_date_graph <- TimeseriesGraph(timeseries_data = timeseries_data,
                                           variable = "REPORT_TOTAL_CONFIRMED",
-                                          date_column = "date")+
-    geom_line(color = colors[1])
+                                          date_column = "date",
+                                          line_color = colors[1])
   
   # get timeseries graph for confirmed cases by onset date
   cases_by_onset_date_graph <- TimeseriesGraph(timeseries_data = timeseries_data,
                                                 variable = "ONSET_TOTAL_CONFIRMED",
-                                                date_column = "date")+
-    geom_line(color = colors[2])
+                                                date_column = "date",
+                                               line_color = colors[2])
   
   # get timeseries graph for hospitalizations by admission date
   hospitalizations_by_admission_date_graph <- TimeseriesGraph(timeseries_data = timeseries_data,
                                                 variable = "ADMISSION_REPORTED_DISEASE",
-                                                date_column = "date")+
-    geom_line(color = colors[3])
+                                                date_column = "date",
+                                                line_color = colors[3])
   
   # display all three graphs
   gridExtra::grid.arrange(cases_by_report_date_graph, cases_by_onset_date_graph, hospitalizations_by_admission_date_graph)
--- sormas-stats-sitrep/sitrep-app/utils/TimeseriesGraph.R
+++ sormas-stats-sitrep/sitrep-app/utils/TimeseriesGraph.R
@@ -3,6 +3,8 @@
 #' @param timeseries_data Output of the AggregateCountsByDate function.
 #' @param variable Variable that should be plotted, must be a column of timeseries_data.
 #' @param date_column Date column, must be a column of timeseries_data.
+#' @param line_color Color of the line of the graph, must be a character string
+#' corresponding to one of the colors recognized by ggplot.
 #'
 #' @return Returns a timeseries graph of the entire time period which is in the 
 #' date_column.
@@ -10,11 +12,11 @@
 #' @export
 #'
 #' @examples
-TimeseriesGraph <- function(timeseries_data, variable, date_column){
+TimeseriesGraph <- function(timeseries_data, variable, date_column, line_color = "steelblue"){
   
   #Timeseries graph
   timeseries_graph <- ggplot2::ggplot(timeseries_data, aes(x=.data[[date_column]], y = .data[[variable]])) +
-    geom_line(size =1.5)+
+    geom_line(size =1, color = line_color)+
     labs(x="", y="")+
     scale_x_date(date_breaks = "3 months")
   
