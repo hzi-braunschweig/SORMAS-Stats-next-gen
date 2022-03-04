2a0c7a4c788a1930912d22f2edafbb9026dc72d7
bernardsilenou <bernard.silenou@helmholtz-hzi.de>
2022-03-04 21:22:10 +0100

    copy the right renv file in docker base file
    Conflicts:
    	sormas-stats-shinyapp/sormas-stats-app/global.R

--- sormas-stats-shinyapp/sormas-stats-app/global.R
+++ sormas-stats-shinyapp/sormas-stats-app/global.R
@@ -1,20 +1,21 @@
-
-# Defining parameters to be used to sample data
-delay = 365 # number of days to count backward from today for entities without specific delay parameters,  default = 90 days in the past
-event_delay = 365  # delay for events
+# Defining parameters to be used to sample data from external database (sormas database)
+delay = 700 # number of days to count backward from today for entities without specific delay parameters,  default = 90 days in the past
+event_delay = 700  # delay for events
 fromDate = as.character(Sys.Date() - delay - 1) #  you can directly define fromDate as: fromDate = as.character("yyyy-mm-dd")
 toDate = as.character(Sys.Date() + 1) # or toDate = as.character("yyyy-mm-dd"), +1 is added because between sql commant does not consider end of intervals
 uniquePersonPersonContact = TRUE # or FALSE to keep only one contact between the same 2 persons ( case person and contact person)
 event_fromDate = as.character(Sys.Date() - event_delay - 1) #  you can directly define fromDate as: fromDate = as.character("yyyy-mm-dd")
 event_toDate = as.character(Sys.Date() + 1) # or toDate = as.character("yyyy-mm-dd"), +1 is added because between sql commant does not consider end of intervals
 
+# defining "Report date" delay to be used as default on front end
+delay_default_UI = 700 # This value should be changed to say 14 on production servers
 # Defining colours to be used by dashboard icons for entities
 colCont = "green"  # contact colour
 colCase="red"   # case colour
 colEvent = "blue"   # event colour
 colPerson = "black"   # contact person or event participant person colour
 colEdge = "black"
-
+ 
 # Defining connection to db
 DB_USER = "sormas_user"
 DB_PASS = "password"
