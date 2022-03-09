d88155dc1d9ad062e331f0c2eccc5172692a5ce2
Frank Hauptmann <f.hauptmann@in2solutions.de>
2022-03-09 16:55:07 +0100

    HSP-1911 try to build one single image

---
+++ sormas-stats-shinyapp/run_app.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sed -i "s/DB_USER =.*/DB_USER = ${DB_USER}/" /app/sormas-stats-app/global.R
+sed -i "s/DB_PASS =.*/DB_PASS = ${DB_PASS}/" /app/sormas-stats-app/global.R
+sed -i "s/DB_HOST =.*/DB_HOST = ${DB_HOST}/" /app/sormas-stats-app/global.R
+sed -i "s/DB_PORT =.*/DB_PORT = ${DB_PORT}/" /app/sormas-stats-app/global.R
+sed -i "s/DB_NAME =.*/DB_NAME = ${DB_NAME}/" /app/sormas-stats-app/global.R
+
+/usr/local/bin/R -e "shiny::runApp('/app', host = '0.0.0.0', port = 3839)
