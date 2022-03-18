c7ea327052c65bb6e940d9cd7c7206800d35814d
bernardsilenou <bernard.silenou@helmholtz-hzi.de>
2022-03-18 16:55:24 +0100

    [#127] added configuration to deactivate user authentication

--- sormas-stats-shinyapp/sormas-stats-app/utils_source_code/userExport.R
+++ sormas-stats-shinyapp/sormas-stats-app/utils_source_code/userExport.R
@@ -1,15 +1,23 @@
-userExport = function(sormas_db){
-  # This function get the user table in sormas, Hashing Passwords with sodium and export 
-  # This username with hased passwords are then used for autentification in sormas-stats  
+userExport = function(sormas_db, authenticat_user){
+  # This function get the user table in sormas, Hashing Passwords with sodium and export to sormas-stats
+  # This username with hashed passwords are then used for authentication in sormas-stats  
   # refs: 
   # https://paulc91.github.io/shinyauthr/
   # https://doc.libsodium.org/password_hashing/
   
   #loading user table
-  queryUsers <- paste0("SELECT uuid, username 
+
+  if(authenticat_user==FALSE){
+    # create a dummy user account with ars as user name and sormas-stats as password
+    users = data.frame(username = "ars", uuid= "sormas-stats" )
+  }else{
+    # extracting users from sormas
+    queryUsers <- paste0("SELECT uuid, username 
                         FROM public.users
                        WHERE active = TRUE")
-  users = dbGetQuery(sormas_db,queryUsers)
+    users = dbGetQuery(sormas_db,queryUsers)
+  }
+  # hash user password
   ret = users %>%
     dplyr::mutate(password = purrr::map_chr(substr(uuid,1,13), sodium::password_store),  .keep = "unused") # hassing
   return(ret)
