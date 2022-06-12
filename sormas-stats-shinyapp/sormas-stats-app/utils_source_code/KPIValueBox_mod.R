f5fe2dd37123a18f2a9921675895df06b0f0c87d
vh94 <valentinhaberl94@gmail.com>
2022-06-12 23:29:39 +0200

    added infobox module

--- sormas-stats-shinyapp/sormas-stats-app/utils_source_code/KPIValueBox_mod.R
+++ sormas-stats-shinyapp/sormas-stats-app/utils_source_code/KPIValueBox_mod.R
@@ -1,4 +1,8 @@
-# kpi MODULE 
+# Render infoboxes for multiple panels (contact,event,case,samples)
+# input data= reactive data,
+# create a list of values to render in the function using the rendervalueBoxfromList function
+# icons and colors arguments to rendervalueBoxfromList can be either single character variables 
+# or be specifically assigned by passing named list items
 KPIValueBox_UI <- function(id) {
   ns <- NS(id)
   tagList(
@@ -12,53 +16,33 @@ KPIValueBox_Server <-
         function(input, output, session) {
         output$boxlist_UI <-
           renderUI({
-           
-  ###'  * Contact Panel *
+          ###' * Contact Panel *
               if (panel=="contact") {
-                
-                ### icons for mean median min max COntacts per case id
-                # icon  = list("Min cont-per-case"="greater-than-equal",
-                #              "Med cont-per-case"="minus",
-                #              "Mean cont-per-case"="minus",
-                #              "Max cont-per-case"="less-than-equal"
-                #              )
-                # color = c("olive","maroon","maroon","navy")
-                # 
+      
+                # icon  = list("Min cont-per-case"="greater-than-equal","Med cont-per-case"="minus",
+                #              "Mean cont-per-case"="minus","Max cont-per-case"="less-than-equal")                 
                 # idData =  data.frame(as.table(summary(as.factor(data()$caze_id), maxsum = 5000000)))
-                # 
-                # ### calculate mean min max contact per case id
-                # KPI <- as.list(
-                #   c(
-                #     "Min cont-per-case" = min(idData$Freq),
-                #     "Med cont-per-case" = median(idData$Freq),
-                #     "Mean cont-per-case" = round(mean(idData$Freq),2),
-                #     "Max cont-per-case" = max(idData$Freq)
-                #     
-                #   )
-                # )
+                # KPI <- as.list( c(
+                #     "Min cont-per-case" = min(idData$Freq), "Med cont-per-case" = median(idData$Freq),
+                #     "Mean cont-per-case" = round(mean(idData$Freq),2),"Max cont-per-case" = max(idData$Freq) ) )
                 KPI_tabel<-as.list(
                   c( 
                     "All contacts" = nrow(data()),
                     table(data()$contactclassification),
                     table(data()$contactstatus)
-                   
                     )
                   )
                 ## render Value boxes
                 box(collapsible = TRUE,
                     width=12,
                     title=NULL,
-              #  rendervalueBoxfromList(KPI,icon,color),
                 rendervalueBoxfromList(KPI_tabel,icon="handshake",color="light-blue")
-                
                 )
               }
-  ###'     * Event Panel *
+          ###' * Event Panel *
               else if(panel=="event") {
-
                 icon  = list("cogs","user-cog","procedures","CLUSTER"="people-arrows","EVENT"="cog","SIGNAL"="exclamation","SCREENING"="vials")
-                color = list("blue", "blue", "red","CLUSTER"="blue","EVENT"="blue","SIGNAL"= "yellow","SCREENING"="navy")
-                
+                color = list("blue", "blue", "red","CLUSTER"="blue","EVENT"="blue","SIGNAL"= "yellow","SCREENING"="navy")      
                 ### build KPI list
                 KPI <- as.list(
                    c(
@@ -68,26 +52,14 @@ KPIValueBox_Server <-
                     table(data()$eventstatus)
                   )
                 )
-                
-              #  icon2 = list("CLUSTER"="people-arrows","EVENT"="cog","SIGNAL"="exclamation","SCREENING"="vials")
-              #  color2=list("CLUSTER"="blue","EVENT"="blue","SIGNAL"= "yellow","SCREENING"="navy")
-                # KPI_from_table<-as.list(
-                #   c(
-                #     table(data()$eventstatus)
-                #   )
-                # )
-                ## render Value boxes
+                ## render event  Value boxes
                 box(collapsible = TRUE,
                     width=12,
                     title=NULL,
-                rendervalueBoxfromList(
-                  KPI,icon,color)#,
-                # rendervalueBoxfromList(
-                #   KPI_from_table,icon=icon2, color=color2 )
+                rendervalueBoxfromList(KPI,icon,color)
                  )
               }
-  
-  ###'      * Case Panel *
+          ###' * Case Panel *
               else if(panel=="case") {
                 icon  = "procedures"
                 color = "red"
@@ -96,10 +68,14 @@ KPIValueBox_Server <-
                   c("All cases" = nrow(data()), table(data()$caseclassification)))
                 quarantine <- as.list(
                   c(table(data()[!is.na(data()$quarantine), ]$quarantine),
-                    "Missing" = nrow(data()[is.na(data()$quarantine),])))
+                    "Missing" = nrow(data()[is.na(data()$quarantine),])
+                    )
+                  )
                 gender <- as.list(
                   c( table(data()[!is.na(data()$sex),]$sex),
-                                     "Missing sex" = nrow(data()[is.na(data()$sex),] )))
+                    "Missing sex" = nrow(data()[is.na(data()$sex),])
+                    )
+                  )
                 age <- as.list(
                   c(
                   "Min age" = min(data()$age[!is.na(data()$age) ]),
@@ -107,71 +83,49 @@ KPIValueBox_Server <-
                   "Mean age" = round(mean(data()$age[!is.na(data()$age) ]),2),
                   "Max age" = max(data()$age[!is.na(data()$age) ]),
                   "Missing age" = nrow(data()$age[is.na(data()$age) ])
-                ))
-                outcome<-as.list(
-                  c(table(data()$outcome)))
-                # occupation<-as.list(
-                #   c(
-                #   table(data()[!is.na(data()$occupationtype), ]$occupationtype),
-                #   "Missing" = nrow(data()[!is.na(data()$occupationtype), ])
-                # ))
-        
+                  )
+                )
+                outcome<-as.list( c(table(data()$outcome) ))
                 ## render Value boxes in tab Box
-              tabBox( id = "tabset1",
+              tabBox( id = "tabsetCase",
                       width = 12,
                       title = icon("procedures"),
                       height = "250px",
                   tabPanel("classification","", rendervalueBoxfromList(classification,icon,color)),
                   tabPanel( "quarantine", "", rendervalueBoxfromList(quarantine,
-                                                                     icon=list("home","hospital","minus","home","question","minus"),
-                                                                     color)),
+                                                                     icon=list("HOME"= "home","INSTITUTIONELL"="hospital",
+                                                                               "NONE"  ="minus","OTHER"= "home","UNKNOWN" ="question",
+                                                                               "Missing" = "minus"), color)),
                   tabPanel( "gender", "", rendervalueBoxfromList(gender,icon,color)),
                   tabPanel( "age", "",
                             rendervalueBoxfromList(age,
-                                                  icon  = list("Min cont-per-case"="greater-than-equal",
-                                                                "Med cont-per-case"="minus",
-                                                                "Mean cont-per-case"="minus",
-                                                                "Max cont-per-case"="less-than-equal"
-                                                              ),
-                                                  color)
-                            ),
-                  tabPanel( "outcome","",  rendervalueBoxfromList(outcome,icon=list("","procedures","handshake","question"),color))
-                #  tabPanel("occupation","",  rendervalueBoxfromList(occupation,icon="users",color))
-              
-                )# close tabBox
+                                                  icon  = list("Min age"="greater-than-equal",
+                                                                "Med age"="minus",
+                                                                "Mean age"="minus",
+                                                                "Max age"="less-than-equal" ), color)),
+                  tabPanel( "outcome","",  rendervalueBoxfromList(outcome,icon=list("procedures","procedures","handshake","question"),color))
+                )
               }
-
-  ###' * samples Panel*
+          ###' * Samples Panel*
               else if(panel=="samples"){
                 icon= "vial"
                 color = "purple"
                 KPI<- as.list(
                      c(
                     "Total Samples"=nrow(data()),
-                    table(data()$case_classification)#,
-                  #  table(data()$samplepurpose),
-                   # 
+                    table(data()$case_classification)
                     )
                   )
                 tabBox(
-                       id = "tabset1",
+                       id = "tabsetSamples",
                        width = 12,
                        title = icon("vials"),
                        height = "250px",
                       tabPanel("classification","", rendervalueBoxfromList( KPI, icon,color)),
-                      tabPanel("result","" ,rendervalueBoxfromList(
-                        as.list(
-                            c(
-                              table(data()$pathogentestresult)
-                              )
-                            ),icon,color
-                        )
+                      tabPanel("result","" ,rendervalueBoxfromList( as.list(c(table(data()$pathogentestresult))),icon,color))
                       )
-                )
               }
-                
-              
-        })
-      }
+          }) #end of render UI
+        }
     )
   }
