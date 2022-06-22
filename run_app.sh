50164c7af2d77173c2ae243506001300bf49e233
Frank Hauptmann <f.hauptmann@in2solutions.de>
2022-06-22 11:45:19 +0200

    fix path to global.R

--- run_app.sh
+++ run_app.sh
@@ -5,8 +5,8 @@ sed -i "s/DB_PASS =.*/DB_PASS = ${DB_PASS}/" /srv/shiny-server/sormas-stats-shin
 sed -i "s/DB_HOST =.*/DB_HOST = ${DB_HOST}/" /srv/shiny-server/sormas-stats-shinyapp/global.R
 sed -i "s/DB_PORT =.*/DB_PORT = ${DB_PORT}/" /srv/shiny-server/sormas-stats-shinyapp/global.R
 sed -i "s/DB_NAME =.*/DB_NAME = ${DB_NAME}/" /srv/shiny-server/sormas-stats-shinyapp/global.R
-sed -i "s/event_delay =.*/event_delay = ${EVENT_DELAY}/" /app/sormas-stats-app/global.R
-sed -i "s/delay =.*/delay = ${DELAY}/" /app/sormas-stats-app/global.R
-sed -i "s/delay_default_UI =.*/delay_default_UI = ${DELAY_DEFAULT_UI}/" /app/sormas-stats-app/global.R
+sed -i "s/event_delay =.*/event_delay = ${EVENT_DELAY}/" /srv/shiny-server/sormas-stats-shinyapp/global.R
+sed -i "s/delay =.*/delay = ${DELAY}/" /srv/shiny-server/sormas-stats-shinyapp/global.R
+sed -i "s/delay_default_UI =.*/delay_default_UI = ${DELAY_DEFAULT_UI}/" /srv/shiny-server/sormas-stats-shinyapp/global.R
 
 /usr/bin/shiny-server
