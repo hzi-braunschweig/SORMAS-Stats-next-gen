6ac1189d65c9e4929db2240f07793c728c9642e1
Frank Hauptmann <f.hauptmann@in2solutions.de>
2022-06-22 11:42:24 +0200

    modify the right run_app.sh

--- run_app.sh
+++ run_app.sh
@@ -6,5 +6,7 @@ sed -i "s/DB_HOST =.*/DB_HOST = ${DB_HOST}/" /srv/shiny-server/sormas-stats-shin
 sed -i "s/DB_PORT =.*/DB_PORT = ${DB_PORT}/" /srv/shiny-server/sormas-stats-shinyapp/global.R
 sed -i "s/DB_NAME =.*/DB_NAME = ${DB_NAME}/" /srv/shiny-server/sormas-stats-shinyapp/global.R
 sed -i "s/event_delay =.*/event_delay = ${EVENT_DELAY}/" /app/sormas-stats-app/global.R
+sed -i "s/delay =.*/delay = ${DELAY}/" /app/sormas-stats-app/global.R
+sed -i "s/delay_default_UI =.*/delay_default_UI = ${DELAY_DEFAULT_UI}/" /app/sormas-stats-app/global.R
 
 /usr/bin/shiny-server
