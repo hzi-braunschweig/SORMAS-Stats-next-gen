c600a50fd44eb9806750a0d5bd0c5d89e6fe0bfd
bernardsilenou <bernard.silenou@helmholtz-hzi.de>
2023-05-12 02:24:40 +0200

    [#178] upgraded lock file

--- sormas-stats-shinyapp/sormas-stats-app/launch_app.R
+++ sormas-stats-shinyapp/sormas-stats-app/launch_app.R
@@ -6,7 +6,7 @@
  renv::restore()  # to restore the state and versions of packages of your project from renv.lock OR to revert back to previous state 
 # renv::init()   # to initialize, do not run in active session
 # renv::init(bare = TRUE) # initialize the project without attempting to discover and install R package dependencies
-# renv::snapshot()  #  save the state of your project to renv.lock  , do not run in active session 
+# renv::snapshot()  #  save the state of your project to renv.lock, do not run in active session
 
 shiny::runApp(appDir = getwd(), launch.browser = TRUE, host = "127.0.0.1") 
 
