54c48509fac34db18eca5ae188fcce978e2d6a53
bernardsilenou <bernard.silenou@helmholtz-hzi.de>
2023-05-18 02:44:19 +0200

    [#178] upgraded R version and deleted archived utils

--- sormas-stats-shinyapp/sormas-stats-app/launch_app.R
+++ sormas-stats-shinyapp/sormas-stats-app/launch_app.R
@@ -7,6 +7,7 @@
 # renv::init()   # to initialize, do not run in active session
 # renv::init(bare = TRUE) # initialize the project without attempting to discover and install R package dependencies
 # renv::snapshot()  #  save the state of your project to renv.lock, do not run in active session
+# renv::install()  # install packages in project library using those in user library
 
 shiny::runApp(appDir = getwd(), launch.browser = TRUE, host = "127.0.0.1") 
 
